1899b4e087d05012769464f4c4b3bd3d35e21237
OrangeFrodo <daxoja@gmail.com>
2022-02-14 14:24:46 +0200

    Comics "Last stage"

--- src/components/Comic.tsx
+++ src/components/Comic.tsx
@@ -1,5 +1,5 @@
 import React, { Component, Props, useState } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 // Firebase
 import { utils } from '@react-native-firebase/app';
@@ -12,37 +12,47 @@ export class Comic extends Component {
         super(props)
 
         this.state = {
-            imageUrl: null
+            imageUrl: null,
+            press: 0
         }
+
+        this.pressed = this.pressed.bind(this)
     }
 
+    pressed = () => {
+        this.setState({
+            press: this.state.press + 1
+        })
+    }
 
     async componentDidMount() {
         // Load image URL
-        let image_url = storage().refFromURL('gs://voidalpha-d42d3.appspot.com/01.jpg')
-        
+        let image_url = storage().refFromURL('gs://voidalpha-d42d3.appspot.com/The Walking Dead/Episode 1/01.jpg')
+
         // Get image URL
         await image_url.getDownloadURL()
-        .then((response) => {
-            console.log("RESPONSE")
-            console.log(response)
-
-            this.setState({ imageUrl: response })
-        })
-        .catch((error) => {
-            console.log("ERROR")
-            console.log(error)
-        })
+            .then((response) => {
+                this.setState({ imageUrl: response })
+            })
+            .catch((error) => {
+                console.log("ERROR")
+                console.log(error)
+            })
     }
 
 
     render() {
-        const  { imageUrl }  = this.state
+        const { imageUrl } = this.state
 
         return (
-            <View>
+            <TouchableOpacity
+                style={styles.headline}
+                onPress={this.pressed}
+            >
+                <Text>The Walking Dead</Text>
                 <Image style={styles.headline} source={{ uri: imageUrl }} />
-            </View>
+            </TouchableOpacity>
+
         );
     }
 }
